c32752fdb61175daf8cc7fce0b8f59b4aee36cfa
LWKYM99 <kiptoroeliakhim@gmail.com>
2026-03-19 01:41:39 +0300

    Add deployment config

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -88,4 +89,4 @@ def announcements():
     return render_template('announcements.html', announcements=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
